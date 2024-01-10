cd2e9dcdeadf59c34818074f0b579883285bf9bb
Alexander <sanchello2009@gmail.com>
2024-01-10 22:51:54 +0100

    TRIVIAL: added logger and config layers

--- projects/eternal-network/cmd/core/main.go
+++ projects/eternal-network/cmd/core/main.go
@@ -3,43 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
 
+	"eternal_network/middleware/request_id"
 	"eternal_network/middleware/request_logger"
-	"eternal_network/packages/config"
-	"eternal_network/packages/logger"
+	"eternal_network/pkg/config"
+	"eternal_network/pkg/logger"
 
 	"github.com/gorilla/mux"
 )
 
-func initConfig() *config.Config {
-	cfg := config.NewConfig()
-
-	cfg.InitConfigByEnv()
-
-	return cfg
-}
-
 func main() {
-	cfg := initConfig()
+	cfg := config.GetConfig()
 
-	logger.InfoLogger.Println(*cfg)
+	logger.Debug(*cfg)
 
 	r := mux.NewRouter()
 
-	r.Use(request_logger.LoggingMiddleware)
+	r.Use(request_id.RequestIdMiddleware, request_logger.LoggingMiddleware)
 
 	// Обработчик запроса
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		for i := 0; i < 1000000; i++ {
-			time.Sleep(1000)
-		}
 		fmt.Fprint(w, "Hello, world!")
 	}).Methods("GET")
 
 	http.Handle("/", r)
 
-	logger.ErrorLogger.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", cfg.Port), nil))
+	logger.Infof("App started on port: %v. Environment: %s\n", cfg.Port, cfg.AppEnv)
 
-	logger.InfoLogger.Printf("App started on port: %v. Environment: %s\n", cfg.Port, cfg.AppEnv)
+	logger.Fatalf("App is crashed: %s", http.ListenAndServe(fmt.Sprintf(":%v", cfg.Port), nil))
 }
--- projects/eternal-network/middleware/request_id/request_id.go
+++ projects/eternal-network/middleware/request_id/request_id.go
@@ -1,2 +1,30 @@
 package request_id
 
+import (
+	"context"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+type RequestIDKey string
+
+var RequestID RequestIDKey = "requestID"
+
+func RequestIdMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestId := r.Header.Get("X-Request-ID")
+
+		if requestId == "" {
+			requestId = uuid.New().String()
+
+			r.Header.Set("X-Request-ID", requestId)
+		}
+
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, RequestID, requestId)
+		r = r.WithContext(ctx)
+
+		next.ServeHTTP(w, r)
+	})
+}
