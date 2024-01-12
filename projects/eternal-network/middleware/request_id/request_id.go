d8bcdd0e59115263683521aaf70ec53d981d29c9
Alexander <sanchello2009@gmail.com>
2024-01-12 23:29:39 +0100

    feat(en): added pr workflow

--- projects/eternal-network/middleware/request_id/request_id.go
+++ projects/eternal-network/middleware/request_id/request_id.go
@@ -11,19 +11,23 @@ type RequestIDKey string
 
 var RequestID RequestIDKey = "requestID"
 
+const XReqIdKey = "X-Request-ID"
+
 func RequestIdMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		requestId := r.Header.Get("X-Request-ID")
+		requestId := r.Header.Get(XReqIdKey)
 
 		if requestId == "" {
 			requestId = uuid.New().String()
 
-			r.Header.Set("X-Request-ID", requestId)
+			r.Header.Set(XReqIdKey, requestId)
 		}
 
 		ctx := r.Context()
 		ctx = context.WithValue(ctx, RequestID, requestId)
 		r = r.WithContext(ctx)
+
+		w.Header().Add(XReqIdKey, requestId)
 
 		next.ServeHTTP(w, r)
 	})
