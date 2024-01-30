b8ee763d6f2aadce199ca1bb363d67a161c67bb6
Alexander <sanchello2009@gmail.com>
2024-01-31 00:50:19 +0100

    tests(en): added request id test

--- projects/eternal-network/middleware/request_id/request_id.go
+++ projects/eternal-network/middleware/request_id/request_id.go
@@ -11,23 +11,23 @@ type RequestIDKey string
 
 var RequestID RequestIDKey = "requestID"
 
-const XReqIdKey = "X-Request-ID"
+const XRequestIdKey = "X-Request-ID"
 
 func RequestIdMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		requestId := r.Header.Get(XReqIdKey)
+		requestId := r.Header.Get(XRequestIdKey)
 
 		if requestId == "" {
 			requestId = uuid.New().String()
 
-			r.Header.Set(XReqIdKey, requestId)
+			r.Header.Set(XRequestIdKey, requestId)
 		}
 
 		ctx := r.Context()
 		ctx = context.WithValue(ctx, RequestID, requestId)
 		r = r.WithContext(ctx)
 
-		w.Header().Add(XReqIdKey, requestId)
+		w.Header().Add(XRequestIdKey, requestId)
 
 		next.ServeHTTP(w, r)
 	})
--- projects/eternal-network/middleware/request_id/request_id_test.go
+++ projects/eternal-network/middleware/request_id/request_id_test.go
@@ -9,7 +9,7 @@ import (
 func TestWithNewReqId(t *testing.T) {
 	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		reqIdCtx, ok := r.Context().Value(RequestID).(string)
-		reqIdW := w.Header().Get(XReqIdKey)
+		reqIdW := w.Header().Get(XRequestIdKey)
 
 		if !ok || reqIdCtx == "" {
 			t.Errorf("Context request id must be set")
@@ -27,6 +27,35 @@ func TestWithNewReqId(t *testing.T) {
 	handlerToTest := RequestIdMiddleware(nextHandler)
 
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
+
+	handlerToTest.ServeHTTP(httptest.NewRecorder(), req)
+}
+
+func TestWithExistReqId(t *testing.T) {
+	const reqId = "my-unique-req-id"
+
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		reqIdCtx, ok := r.Context().Value(RequestID).(string)
+		reqIdW := w.Header().Get(XRequestIdKey)
+
+		if !ok || reqIdCtx != reqId {
+			t.Errorf("Context request id is missed")
+		}
+
+		if reqIdW != reqId {
+			t.Errorf("Response X-Request-Id must be set")
+		}
+
+		if ok && reqIdCtx != reqIdW {
+			t.Errorf("Context and response request id must be equal")
+		}
+	})
+
+	handlerToTest := RequestIdMiddleware(nextHandler)
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+
+	req.Header.Set(XRequestIdKey, reqId)
 
 	handlerToTest.ServeHTTP(httptest.NewRecorder(), req)
 }
